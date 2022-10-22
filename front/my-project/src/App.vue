aa116032457e7ed94cebec80815c23a36af9d8f0
hnakashiro <redpikachu321@gmail.com>
2022-10-22 13:54:31 +0900

    とりあえずの成果

---
+++ front/my-project/src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  
+  <div id="app">
+    <div id="logo">
+      <router-link to='/'>
+       <img alt="Airdapt" height="80" src="./assets/Airdapt_logo.png">
+      </router-link>
+    </div>
+    
+    <MainHeader/>
+    <router-view/>
+    <div id="map">
+     <google-map/>
+    </div>
+    <tab-menu></tab-menu>
+    
+  </div>
+</template>
+
+<script>
+
+import TabMenu from '@/components/TabMenu'
+import MainHeader from '@/components/MainHeader'
+import GoogleMap from './components/GoogleMap.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+    TabMenu, MainHeader, GoogleMap
+  }
+
+  
+  
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 40px;
+  font-size: 8px;
+  
+}
+#logo {
+  text-align: left;
+  margin-top: -15px;
+  
+}
+#map {
+  margin-top: 30px;
+}
+</style>
---
+++ front/my-project/src/components/HelloWorld.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    
+  </div>
+  
+</template>
+
+<script>
+
+import TabMenu from '@/components/TabMenu'
+
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: '地図をなぞって、雰囲気検索。'
+    }
+  },
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-family: 'Yusei Magic', sans-serif;
+  font-weight: bold;
+  text-align: left;
+  margin-left:20px;
+  font-size: 18px;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
